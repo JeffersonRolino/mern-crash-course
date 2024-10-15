3928329501477f8148c0bc1d1de72464c778c8cc
jeffersonrolino <jefferson.rolino@gmail.com>
2024-10-15 03:59:39 -0300

    feat: create product delete method

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,18 @@ app.post("/api/products", async (req, res) => {
   }
 });
 
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Product deleted" });
+  } catch (error) {
+    console.error("Error in Delete product: ", error.message);
+    res.status(500).json({ success: false, message: "Product Not Found" });
+  }
+});
+
 app.listen(5000, () => {
   connectDB();
   console.log("Server started at http://localhost:5000");
